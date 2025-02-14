bd92771a2af79a07215960f9bbd8af80b0fa0a0e
Adrian <adunham95@gmail.com>
2025-02-14 08:02:12 -0500

    Updated to use prisma type

--- src/lib/server/db/prisma.ts
+++ src/lib/server/db/prisma.ts
@@ -1,16 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-import { PrismaClient } from '../../../../prisma/generated/client';
+import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
+const prisma = new PrismaClient();
 
-if (process.env.NODE_ENV === 'production') {
-	prisma = new PrismaClient();
-} else {
-	if (!global.prisma) {
-		global.prisma = new PrismaClient();
-	}
-	prisma = global.prisma;
-}
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
