21fb4ca5052f14b39f5f2a2cc9d94a2da9b7aae6
Adrian <adunham95@gmail.com>
2025-03-06 21:52:39 -0500

    Playing with hub page

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	let { children } = $props();
+	let { children, className = '' } = $props();
 </script>
 
-<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-4">
+<div class={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-4 ${className}`}>
 	{@render children()}
 </div>
--- src/routes/(app)/hub/+page.svelte
+++ src/routes/(app)/hub/+page.svelte
@@ -1,10 +1,21 @@
 <script>
 	import Container from '$lib/components/Container.svelte';
 	import Note from '$lib/components/Forms/Note.svelte';
+
+	const { data } = $props();
+	$inspect(data);
 </script>
 
-<Container>
-	<div class="flex justify-center">
+<Container className="max-w-4xl grid grid-cols-5 gap-4">
+	<div class="card col-span-5">
+		<div class="p-4 flex justify-between items-center">
+			<h2 class=" text-4xl font-bold">Hello {data.user.firstName}!</h2>
+			<div>
+				<a href="/resumes" class=" rounded-sm border-2 text-sm p-2"> Resume </a>
+			</div>
+		</div>
+	</div>
+	<div class="flex justify-center col-span-2">
 		<div class=" w-full max-w-[500px] card">
 			<div class="card-body">
 				<Note />
